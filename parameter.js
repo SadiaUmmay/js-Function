e565a79f1605f67248735ac4c1f9847e6cbc2f1a
unknown <ummayjannatsadia@gmail.com>
2025-08-08 16:35:06 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,32 @@
+function vaatKhao(){
+    console.log('hat dho')
+    console.log('bosho')
+    console.log('khabar niye khao')
+}
+vaatKhao();
+
+// .........................
+
+function square(number){
+    console.log('value=',number);
+    const borgo = number * number
+    console.log("square result=" ,borgo)
+}
+square(12);
+square(2);
+
+// ............................
+
+function sum(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum)
+}
+sum(2,4);
+
+// .........................
+
+function addAll (a,b,c,d,e){
+    const total = a+b+c+d+e;
+    console.log(total)
+}
+addAll(1,2,3,4,5);

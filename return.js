6c8f7197dd4fa8c4fe2c42dc24cabdc59a553c6b
unknown <ummayjannatsadia@gmail.com>
2025-08-08 17:20:34 +0600

    conditional return

---
+++ return.js
@@ -0,0 +1,42 @@
+function tenTimes (number){
+    const result = number*10;
+    // console.log(result)
+    return result;
+}
+// .............
+
+function cutHalf(number){
+    const half = number/ 2;
+    return half;
+
+}
+tenTimes(5);
+const output = tenTimes(50)
+// console.log(output)
+
+const num2 = tenTimes(99)
+
+// ................
+
+function add2 (price1, price2){
+    return price1 + price2;
+}
+const bill = add2(34,23);
+console.log(bill)
+
+// .................
+
+function doMath(num1, num2){
+    const sum =num1 + num2;
+    const diff = num1 - num2;
+    const multiply = sum * diff;
+    const result = multiply /2;
+    return result;
+
+}
+const result = doMath(10,5);
+console.log(result)
+
+
+// .....................
+

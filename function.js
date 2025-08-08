301e6d20e6baaaf680fa09b92b1034ad105e0035
unknown <ummayjannatsadia@gmail.com>
2025-08-08 16:07:50 +0600

    19-1

---
+++ function.js
@@ -0,0 +1,15 @@
+const number = 45;
+// function declaration 
+function fanOffKor(){
+    console.log('bosha theke uthe dara')
+    console.log('walk towards the switch')
+}
+// call the function
+fanOffKor();
+
+
+function brushYourTeeth(){
+    console.log('pick up the brush')
+    console.log('add paste')
+}
+brushYourTeeth();

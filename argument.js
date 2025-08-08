0e3be2c13d662c9b59e5294ae33e2bbc299c3474
unknown <ummayjannatsadia@gmail.com>
2025-08-08 16:49:09 +0600

    argument

---
+++ argument.js
@@ -0,0 +1,23 @@
+function double(num1){
+    const doubled = num1* 2;
+    console.log(num1, doubled)
+}
+console.log('..................')
+double(5);
+console.log("...................")
+double(788);
+
+const number = 55;
+double(number);
+const money = 23;
+double(money);
+
+// ..............................
+
+function def(num1,num2){
+    const def = num1- num2;
+    console.log(num1,num2, 'difference is = ' , def)
+}
+const fatherAge = 40;
+const myAge =20;
+def(fatherAge,myAge);

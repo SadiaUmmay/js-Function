10a957f818801e40eb00fbc8e542006c0757527a
unknown <ummayjannatsadia@gmail.com>
2025-08-11 23:51:27 +0600

    return task 1

--- task.js
+++ task.js
@@ -1,3 +1,6 @@
-function prepareJuice(){
-    
+function numbers(num1,num2,num3,num4){
+    const result = num1 * num2 * num3 * num4;
+    return result;
 }
+let result= numbers(1,2,3,4);
+console.log(result)
